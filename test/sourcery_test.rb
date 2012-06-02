bdcdd73deacc532e9397196ed5bf900f0f0935c2
Frank Mitchell <me@frankmitchell.org>
2012-06-02 06:40:31 -0700

    Additional test to make sure Sourcery.wxs picks up existing files.

--- test/sourcery_test.rb
+++ test/sourcery_test.rb
@@ -19,4 +19,14 @@ class PackupSourceryTest < Test::Unit::TestCase
     element = REXML::XPath.first(wxs, "//File")
     assert '$(var.Source)\README.md', element.attributes['Source']
   end
+
+  def test_sourcery_finds_existing_files
+    FileUtils.mkpath 'wix/src'
+    FileUtils.copy 'README.md', 'wix/src/README.md'
+    Packup.stuff 'Magic'
+    Rake::Task['wix/Sourcery.wxs'].invoke
+    wxs = REXML::Document.new File.read('wix/Sourcery.wxs')
+    element = REXML::XPath.first(wxs, "//File")
+    assert '$(var.Source)\README.md', element.attributes['Source']
+  end
 end
